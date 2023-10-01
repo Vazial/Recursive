20100035168c6b5d322e3b381089bd3a51ca2065
Vazial <bomubomu1221@Gmail.com>
2023-10-01 13:30:30 +0900

    Commmit

---
+++ Recursive/Integral.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive
+{
+
+    // 参考 : https://atmarkit.itmedia.co.jp/ait/articles/2201/20/news015.html
+    public class Integral
+    {
+        public static double func(double x)
+        {
+            return x * x;
+        }
+
+        public static double NotInte(double min, double max, double step, double integral = 0.0)
+        {
+            for (double ii = min; ii < max; ii += step)
+            {
+                // (f(a) + f(b)) (b - a) / 2
+                integral += (func(ii) + func(ii + step)) * step / 2;
+            }
+            return integral;
+        }
+
+        public static double Inte(double min, double max, double step, double integral = 0.0)
+        {
+            if (min >= max) { return integral; }
+            // (f(a) + f(b)) (b - a) / 2
+            integral += (func(min) + func(min + step)) * step / 2;
+            return Inte((min + step), max, step, integral);
+        }
+    }
+}
---
+++ RecursiveTests/IntegralTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recursive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive.Tests
+{
+    [TestClass()]
+    public class IntegralTests
+    {
+        [TestMethod()]
+        public void NotInteTest()
+        {
+            var result = Integral.NotInte(0, 1, 0.0001);
+            Assert.AreEqual(Math.Round(result, 3), 0.333);
+        }
+
+        [TestMethod()]
+        public void InteTest()
+        {
+            var result = Integral.Inte(0, 1, 0.0001);
+            Assert.AreEqual(Math.Round(result, 3), 0.333);
+        }
+    }
+}
