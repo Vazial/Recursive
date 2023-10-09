1ca93f40a1e6bbaf21972f048af642d3602cab4d
Vazial <bomubomu1221@Gmail.com>
2023-10-09 13:52:06 +0900

    Add Maze

---
+++ Maze/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Maze
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var gen = new MazeGenerator(20, 20);
+            var maze = gen.CreateMaze();
+            MazeGenerator.DebugPrint(maze);
+
+            var pos = Seeker.Seek(maze);
+            MazeGenerator.DebugPrint(maze);
+            Console.WriteLine($"X : {pos.X}, Y : {pos.Y}");
+        }
+    }
+}
